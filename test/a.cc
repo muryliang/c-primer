de521b0814b840bcfcf1066ce1883ab2b6330391
muryliang <muryliang@gmail.com>
2017-10-25 16:54:30 +0800

    update

--- test/a.cc
+++ test/a.cc
@@ -1,9 +1,26 @@
 #include "my.h"
 
+class A {
+    private:
+        int a;
+        int b;
+    public:
+        A(int p, int q): a(p), b(q) { cout << " in A ctor" << endl;}
+        A(const A& b) { a = b.a; cout << " in A copy" << endl;}
+        const int& get() const { return a;}
+        const int& get() {cout << "not const" << endl; return a;}
+};
+
+class B{
+    private:
+        A k;
+    public:
+        B(A& a): k(50, 100) { cout << " in B class" << endl;}
+};
+
 int main() {
-    const char *p = "123456789";
-    string b("abcdefg");
-    string a(p);
-    a.append(b);
-    cout << a << endl;
+    A a{20, 21};
+    cout << "begin" << endl;
+    B b{a};
+    return 0;
 }
